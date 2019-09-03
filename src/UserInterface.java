8e558461e6dff11f69f92a58501606a02a09eb2c
Cortona1 <anthonycorton95@gmail.com>
2019-09-03 13:24:36 -0400

    Initial commit

---
+++ src/UserInterface.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+public class UserInterface {
+
+    private Scanner reader;
+
+
+    public UserInterface(Scanner reader) {
+        this.reader = reader;
+
+    }
+
+
+    public void start() {
+        introductions();
+    }
+
+
+    public void introductions() {
+
+    }
+}
